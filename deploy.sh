4a3d3ae61469468b5bf7c985051985c55ee18bf0
Vladimir Minakov <minakov@gmail.com>
2018-02-22 13:13:41 +0300

    Fixes deploy path

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-gsutil cp single.ipa gs://sw-lobby.appspot.com/apps/${ACCOUNT_ID}/${APP_ID}/single.ipa
+gsutil cp single.ipa gs://sw-lobby.appspot.com/apps/${ACCOUNT_ID}/${APP_ID}/${PROFILE_ID}.ipa
