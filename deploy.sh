5fce8a0dafdabb772a6aabb2d9f06024a305c2fb
Vladimir Minakov <minakov@gmail.com>
2018-03-26 11:45:31 +0300

    Move build artefacts

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-gsutil cp single.ipa gs://sw-lobby.appspot.com/apps/${ACCOUNT_ID}/${APP_ID}/${PROFILE_ID}.ipa
+gsutil cp single.ipa gs://sw-lobby.appspot.com/apps/${ACCOUNT_ID}/${APP_ID}/${BRANCH_ID}/${BUILD_ID}.ipa
