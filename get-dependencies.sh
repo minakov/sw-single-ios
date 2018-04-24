d1dca9802974e3365d475b499108ce9e9dd6e5e5
Vladimir Minakov <minakov@gmail.com>
2018-04-24 15:55:28 +0300

    Append index html

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -3,10 +3,9 @@
 wget $CERTIFICATE_URL -O cert.p12
 wget $PROVISION_URL -O adhoc.mobileprovision
 
-echo "$ARCHIVE_LINK"
 if [ ! "$ARCHIVE_LINK" = "" ]
 then
-  mkdir res && curl -L $ARCHIVE_LINK | tar xzC res --strip 1
+  mkdir res && curl -L $ARCHIVE_LINK | tar xzC single/static --strip 1
 fi
 
 cp .boto ~/.boto
--- single/ViewController.swift
+++ single/ViewController.swift
@@ -1,12 +1,29 @@
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKUIDelegate {
+    var webView: WKWebView!
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        webView.evaluateJavaScript("navigator.userAgent") { [weak webView] (result, error) in
+            if let webView = webView, let userAgent = result as? String {
+                webView.customUserAgent = userAgent + "[NATIVE/WV]"
+            }
+        }
+        self.view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+        if let path = Bundle.main.path(forResource: "index", ofType: "html", inDirectory: "static") {
+            webView.load(URLRequest(url: URL(fileURLWithPath: path)))
+        }
     }
 }
